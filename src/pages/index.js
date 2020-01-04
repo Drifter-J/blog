9171a742964195e258c56a23141176f78d097efe
Jaye <latenytcoffee@gmail.com>
2020-01-05 02:10:28 +0900

    Tried Part 6 of Gatsby Tutorials : Transformer Plugins, but failed to show contents - need to be worked on

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,69 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 import Header from './components/header'
 import Layout from './components/layoutWithGraphQL'
 
 //import Contact from './contact'
 
-export default ({data}) => (
-    <Layout>
-        <Header headerText={"Hello " +  data.site.siteMetadata.title} />
-        <p>Let's Start</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </Layout>
-)
+export default ({ data }) => {
+    console.log(data)
+    return (
+        <Layout>
+            <div>
+                <h1
+                    css={css`
+                        display: inline-block;
+                        border-bottom: 1px solid;
+                    `}
+                >
+                    {data.site.siteMetadata.title}
+                </h1>
+                <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+                {data.allMarkdownRemark.edges.map(({ node }) => (
+                    <div key={node.id}>
+                    <h3
+                        css={css`
+                            margin-bottom: ${rhythm(1/4)};
+                        `}
+                        >
+                            {node.frontmatter.title}{" "}
+                            <span
+                                css={css`
+                                    color: #bbb;
+                                `}
+                            >
+                                — {node.frontmatter.date}
+                            </span>
+                        </h3>
+                        <p>{node.excerpt}</p>
+                    </div>
+                ))}
+            </div>
+        </Layout>
+    )
+}
 
 export const query = (
     graphql `
-        query {
-            site {
-                siteMetadata {
+        query {   
+            allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
+              totalCount
+              edges {
+                node {
+                  frontmatter {
+                    date
                     title
+                  }
                 }
-            }   
-        }
+              }
+            }
+            site {
+              siteMetadata {
+                title
+              }
+            }
+          }
     `
 )
